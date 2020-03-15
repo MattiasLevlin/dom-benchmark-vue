cd7ee826a70cc92e497128eb756417457c5b0c93
Mattias Levlin <mattias.levlin@gmail.com>
2020-03-16 00:38:46 +0200

    Remove svg and nonempty tests

--- src/components/Benchmark.vue
+++ src/components/Benchmark.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="benchmark">
-    <button v-on:click="addEmptyDivs">addEmptyDivs</button>
-    <button v-on:click="removeEmptyDivs">removeEmptyDivs</button>
     <button v-on:click="addDivs">addDivs</button>
+    <button v-on:click="editDivs">addDivs</button>
     <button v-on:click="removeDivs">removeDivs</button>
-    <button v-on:click="addEmptySvgs">addEmptySvgs</button>
-    <button v-on:click="removeEmptySvgs">removeEmptySvgs</button>
-    <button v-on:click="addSvgs">addSvgs</button>
-    <button v-on:click="removeSvgs">removeSvgs</button>
 
     <BenchmarkContainer 
-      :numberOfElements=numberOfElements
       :selectedTest=selectedTest
     />
   </div>
@@ -26,47 +20,19 @@ export default {
   },
   data: function() {
     return {
-      numberOfElements: 0,
       selectedTest: '',
     }
   },
   methods: {
-    // Test 1, 2 empty divs
-    addEmptyDivs: function () {
-      this.numberOfElements = 10000
-      this.selectedTest = 'empty_div'
-    },
-    removeEmptyDivs: function() {
-      this.numberOfElements = 0
-      this.selectedTest = 'empty_div'
-    },
-    // Test 3, 4 nonempty divs
     addDivs: function () {
-      this.numberOfElements = 10000
-      this.selectedTest = 'nonempty_div'
+      this.selectedTest = 'add'
+    },
+    editDivs: function() {
+      this.selectedTest = 'edit'
     },
     removeDivs: function() {
-      this.numberOfElements = 0
-      this.selectedTest = 'nonempty_div'
+      this.selectedTest = 'remove'
     },
-    // Test 5, 6 empty svgs
-    addEmptySvgs: function () {
-      this.numberOfElements = 10000
-      this.selectedTest = 'empty_svg'
-    },
-    removeEmptySvgs: function() {
-      this.numberOfElements = 0
-      this.selectedTest = 'empty_svg'
-    },
-    // Test 7, 8 nonempty svgs
-    addSvgs: function () {
-      this.numberOfElements = 10000
-      this.selectedTest = 'nonempty_svg'
-    },
-    removeSvgs: function() {
-      this.numberOfElements = 0
-      this.selectedTest = 'nonempty_svg'
-    }
   },
 }
 </script>
--- src/components/BenchmarkContainer.vue
+++ src/components/BenchmarkContainer.vue
@@ -1,41 +1,23 @@
 <script>
-import DivEmpty from './DivEmpty.vue'
-import DivNonempty from './DivNonempty.vue'
-import SvgEmpty from './SvgEmpty.vue'
-import SvgNonempty from './SvgNonempty.vue'
 
 export default {
   name: 'BenchmarkContainer',
-  render (createElement) {
-      let elementToCreate = null
-      if (this.$props.selectedTest == "empty_div") {
-        elementToCreate = DivEmpty
-      } else if (this.$props.selectedTest == "nonempty_div") {
-        elementToCreate = DivNonempty
-      } else if (this.$props.selectedTest == "empty_svg") {
-        elementToCreate = SvgEmpty
-      } else if (this.$props.selectedTest == "nonempty_svg") {
-        elementToCreate = SvgNonempty
+  render: function (createElement) {
+    let divToCreate = []
+    if (this.selectedTest === 'add') {
+      
+      for (var i = 0; i < 10000; i += 1) {
+          divToCreate.push(<div
+            class="benchmark"
+            id={i}
+          >
+              <p>asd</p>
+          </div>)
       }
-      return createElement(
-        'div',
-        Array.apply(
-          null, { 
-            length: this.$props.numberOfElements
-          }
-        ).map(
-            () => {
-              return createElement(elementToCreate)
-            }
-        )
-      )
+    }
+      return createElement(divToCreate)
   },
   watch: {
-      numberOfElements: {
-        handler (value) {
-            this.numberOfElements = value;
-        }
-      },
       selectedTest: {
         handler (value) {
           this.selectedTest = value;
@@ -43,7 +25,6 @@ export default {
       }
   },
   props: {
-    numberOfElements: Number,
     selectedTest: String,
   }
 }
