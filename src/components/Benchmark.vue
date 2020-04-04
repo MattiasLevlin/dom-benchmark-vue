039c8990b181bbc9c9b2896ea79e18c12036b55c
Mattias Levlin <mattias.levlin@gmail.com>
2020-04-04 22:23:32 +0300

    Measurements using performance.now, and v-for as for loop implementation

--- src/components/Benchmark.vue
+++ src/components/Benchmark.vue
@@ -1,38 +1,77 @@
 <template>
-  <div class="benchmark">
-    <button v-on:click="addDivs">addDivs</button>
-    <button v-on:click="editDivs">addDivs</button>
-    <button v-on:click="removeDivs">removeDivs</button>
+  <div>
+    <div class="benchmark">
+      <button @click="addDiv()">1 addDiv</button>
+      <button @click="editOneDiv()">2 editOneDiv</button>
+      <button @click="editAllDiv()">3 editAllDiv</button>
+      <button @click="removeOneDiv()">4 removeOneDiv</button>
+      <button @click="removeAllDiv()">5 removeAllDiv</button>
+    </div>
+    <div class="asd">
+        <div
+          v-for="i in divToCreate"
+          v-bind:key="i">
+            <p>Div{{ i }}</p>
+        </div>
+    </div>
 
-    <BenchmarkContainer 
-      :selectedTest=selectedTest
-    />
   </div>
 </template>
 
 <script>
-import BenchmarkContainer from './BenchmarkContainer.vue'
 
 export default {
   name: 'Benchmark',
-  components: {
-    BenchmarkContainer
-  },
-  data: function() {
+  data () {
     return {
-      selectedTest: '',
+      divToCreate: 0,
+      asd1: null,
+      asd2: null
     }
   },
   methods: {
-    addDivs: function () {
-      this.selectedTest = 'add'
+    /* eslint-disable no-console */
+
+    // 1
+    addDiv () {
+      this.divToCreate = 10000
+      console.log('Testing adding 10000 elements')
+      this.asd1 = performance.now()
     },
-    editDivs: function() {
-      this.selectedTest = 'edit'
+    // 2
+    editOneDiv: function() {
+      // this.selectedTest = 'edit'
     },
-    removeDivs: function() {
-      this.selectedTest = 'remove'
+    editAllDiv: function() {
+
     },
+    removeOneDiv: function() {
+
+    },
+    removeAllDiv: function() {
+      this.divToCreate = 0
+      console.log('Testing removing 10000 elements')
+      this.asd1 = performance.now()
+    },
+  },
+    updated: function () {
+      this.asd2 = performance.now()
+      console.log('Time it took was: ' + (this.asd2 - this.asd1) + 'milliseconds')
+    },
+    render: function (createElement) {
+    let divToCreate = []
+    if (this.selectedTest === 'add') {
+      
+      for (var i = 0; i < 10000; i += 1) {
+          divToCreate.push(<div
+            class="benchmark"
+            id={i}
+          >
+              <p>asd</p>
+          </div>)
+      }
+    }
+      return createElement(divToCreate)
   },
 }
 </script>
