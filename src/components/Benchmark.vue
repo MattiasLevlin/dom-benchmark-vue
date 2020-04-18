0201dd2d006b13a48f257d006cfc5fdcccfd6dc0
Mattias Levlin <mattias.levlin@gmail.com>
2020-04-18 22:33:27 +0300

    Re-add array.map implementatioRe-add array.map implementationn

--- src/components/Benchmark.vue
+++ src/components/Benchmark.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    <div class="benchmark">
-      V-FOR
+  <div class="benchmark">
+    <p>Using v-for</p>
       <button @click="addDiv()">1 addDiv</button>
       <button @click="editOneDiv()">2 editOneDiv</button>
       <button @click="editAllDiv()">3 editAllDiv</button>
       <button @click="removeOneDiv()">4 removeOneDiv</button>
       <button @click="removeAllDiv()">5 removeAllDiv</button>
       {{ randomText }}
-    </div>
+          <p>Using array.map()</p>
+    <button
+      v-on:click="add1000Elements"
+    >
+      Add 10,000 elements
+    </button>
+    <button
+      v-on:click="remove1000Elements"
+    >
+      Remove 10,000 elements
+    </button>
+    <BenchmarkContainer 
+      :numberOfElements=numberOfElements
+      :startTimerArrayMap=startTimerArrayMap
+    />
         <div
           id="container"
           v-for="i in divToCreate"
@@ -16,32 +29,30 @@
           v-html="htmlxc(i)"
           :ref="i"
         >
-            <p>Div{{ i }}</p>
-        </div>
-
+        <p>Div{{ i }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-
 /* eslint-disable no-console */
+import BenchmarkContainer from './BenchmarkContainer.vue'
 
 export default {
   name: 'Benchmark',
-  data () {
+  components: {
+    BenchmarkContainer
+  },
+  data: function() {
     return {
       divToCreate: 0,
       startTimer: null,
+      startTimerArrayMap: null,
       stopTimer: null,
+      numberOfElements: 0,
       asdf3: 1,
       randomText: 'asd'
-      // htmlxc: `<p>Message ${i}</p>`
     }
-  },
-  computed: {
-    /* htmlxc: function() {
-      return `<h1>Hello ${ Math.random() }</h1>`;
-    } */
   },
   methods: {
 
@@ -72,10 +83,18 @@ export default {
       this.startTimer = performance.now()
       this.divToCreate = 0
     },
+    add1000Elements: function () {
+      this.startTimerArrayMap = performance.now()
+      this.numberOfElements = 10000
+    },
+    remove1000Elements: function() {
+      this.startTimerArrayMap = performance.now()
+      this.numberOfElements = 0
   },
     updated: function () {
       this.stopTimer = performance.now()
       console.log('Time it took was: ' + (this.stopTimer - this.startTimer) + 'milliseconds')
     },
+  },
 }
 </script>
---
+++ src/components/BenchmarkContainer.vue
@@ -0,0 +1,29 @@
+<script>
+/* eslint-disable no-console */
+export default {
+  name: 'BenchmarkContainer',
+  render (createElement) {
+      return createElement('div',
+      Array.apply(null, { length:
+        this.$props.numberOfElements
+      }).map((e, i) => {
+          return createElement('div', 'div #' + i)
+      }))
+  },
+  watch: {
+      numberOfElements: {
+          handler (val) {
+              this.numberOfElements = val;
+          }
+      }
+  },
+  updated: function () {
+      this.stopTimer = performance.now()
+      console.log('Time it took was: ' + (this.stopTimer - this.$props.startTimerArrayMap) + 'milliseconds')
+    },
+  props: {
+    numberOfElements: Number,
+    startTimerArrayMap: null,
+  }
+}
+</script>
